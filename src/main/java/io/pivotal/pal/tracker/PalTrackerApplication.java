d31dfa47454daab042a8f6555871de6551b7a899
aru74 <aru74web.de>
2021-03-04 16:28:26 +0000

    lab6

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,13 +1,14 @@
 package io.pivotal.pal.tracker;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import javax.sql.DataSource;
+import java.util.TimeZone;
+
+import static java.lang.System.getenv;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -16,8 +17,8 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
 }
